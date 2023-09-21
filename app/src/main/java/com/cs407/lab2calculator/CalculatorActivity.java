588649967c1cb1bee338927960edd7e82f51c5ee
Pinzhe Liu <pliu227@wisc.edu>
2023-09-21 12:26:09 -0500

    add basic functions

---
+++ app/src/main/java/com/cs407/lab2calculator/CalculatorActivity.java
@@ -0,0 +1,21 @@
+package com.cs407.lab2calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+        textView = (TextView) findViewById(R.id.textView);
+        Intent intent = getIntent();
+        int result = intent.getIntExtra("message", 0);
+        textView.setText("The answer is:  " + result);
+    }
+}
--- app/src/main/java/com/cs407/lab2calculator/MainActivity.java
+++ app/src/main/java/com/cs407/lab2calculator/MainActivity.java
@@ -2,7 +2,11 @@ package com.cs407.lab2calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //first number
+    public int x1;
+    //second number
+    public int y1;
+    public void initialize(View view){
+        EditText num1 = (EditText) findViewById(R.id.num1);
+        EditText num2 = (EditText) findViewById(R.id.num2);
+        //Toast.makeText(MainActivity.this, num1.getText().toString(), Toast.LENGTH_LONG).show();
+        //Toast.makeText(MainActivity.this, num2.getText().toString(), Toast.LENGTH_LONG).show();
+        String x = num1.getText().toString();
+        String y = num2.getText().toString();
+        this.x1 = Integer.parseInt(x);
+        this.y1 = Integer.parseInt(y);
+    }
+    public void add(View view) {
+        initialize(view);
+        goToActivity(x1+y1);
+    }
+
+    public void sub(View view) {
+        initialize(view);
+        goToActivity(x1-y1);
+    }
+
+    public void mul(View view) {
+        initialize(view);
+        goToActivity(x1*y1);
+    }
+
+    public void div(View view) {
+        initialize(view);
+        goToActivity(x1/y1);
+    }
+
+    public void goToActivity(double x){
+        Intent intent = new Intent(this, CalculatorActivity.class);
+        intent.putExtra("message", x);
+        startActivity(intent);
     }
 }
