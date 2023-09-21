e48de80a69483d56730c3e09b5f75cf6bb9950f0
Pinzhe Liu <pliu227@wisc.edu>
2023-09-21 12:26:09 -0500

    add partial add

--- app/src/main/java/com/cs407/lab2calculator/MainActivity.java
+++ app/src/main/java/com/cs407/lab2calculator/MainActivity.java
@@ -3,6 +3,9 @@ package com.cs407.lab2calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void add(View view) {
+        EditText num1 = (EditText) findViewById(R.id.num1);
+        EditText num2 = (EditText) findViewById(R.id.num2);
+        Toast.makeText(MainActivity.this, num1.getText().toString(), Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, num2.getText().toString(), Toast.LENGTH_LONG).show();
+        //goToActivity(myTextField.getText().toString());
     }
 }
